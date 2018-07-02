9288c6f9908def9b8e92a37de884ae88758231fc
lizcastroprogrammer <elizabethjcastro42@gmail.com>
2018-07-02 11:30:46 -0700

    added testing and middlewares

---
+++ src/Root.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from 'reducers';
+import async from 'middlewares/async';
+
+export default ({ children, initialState = {} }) => {
+    const store = createStore(reducers, initialState, applyMiddleware(async));
+
+    return (
+        <Provider store={store}>
+            {children}
+        </Provider>
+    );
+};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from 'Root';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/App';
+import App from 'components/App';
 
 ReactDOM.render(
-    <App />, 
+    <Root>
+        <BrowserRouter>
+            <Route path="/" component={App} />
+        </BrowserRouter>
+    </Root>, 
+    //{} represents the initial state which is empty right now
     document.querySelector('#root')
 );
